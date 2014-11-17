8084ae24eba4a15e9322059a962f4454d1288a71
Praphull Kumar <praphull27@gmail.com>
2014-11-16 23:19:08 -0800

    Created an express server.

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen();
+var express = require('express');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.set('port', process.env.PORT || 5000);
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(__dirname + 'public'));
+
+
+app.listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
+});
