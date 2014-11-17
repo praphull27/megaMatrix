d254214ab55c073c53c1df882e1e15f45c32e72d
Praphull Kumar <praphull27@gmail.com>
2014-11-16 23:27:46 -0800

    Final Changes.

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
-var express = require('express');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
+var express = require('express')
 var app = express();
 
-app.set('port', process.env.PORT || 5000);
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(__dirname + 'public'));
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
 
 app.listen(app.get('port'), function() {
-	console.log('Express server listening on port ' + app.get('port'));
-});
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
